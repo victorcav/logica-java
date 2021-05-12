e18d5f7f3d7ee22a6d059c2e1c6ff7ec53292c0f
Entra21 <entra21@proway.treina>
2021-05-12 20:49:27 -0300

    Funcao de Bhaskara

---
+++ src/FuncaoDeBaskara.java
@@ -0,0 +1,32 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class FuncaoDeBaskara {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite o valor de a: ");
+        double a = scanner.nextDouble();
+
+        System.out.println("Digite o valor de b: ");
+        double b = scanner.nextDouble();
+
+        System.out.println("Digite o valor de c: ");
+        double c = scanner.nextDouble();
+
+        double delta = (Math.pow(b, 2.0)) - (4*a*c);
+
+        double x1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+        double x2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+
+        System.out.printf("O valor de X1 é: %f%n", x1);
+        System.out.printf("O valor de X2 é: %f%n", x2);
+
+        scanner.close();
+
+    }
+}
