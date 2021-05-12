f07dcc9804a6913b85f5e7b2863faf672a4bda39
Entra21 <entra21@proway.treina>
2021-05-11 21:10:44 -0300

    Entendendo o processo de Casting

---
+++ src/CalculaAreaTrapezio.java
@@ -0,0 +1,16 @@
+public class CalculaAreaTrapezio {
+
+    public static void main(String[] args) {
+
+        double b, h, B, area;
+        h = 5.0;
+        b = 6.2;
+        B = 8.0;
+
+        area = (b + B) / 2.0 * h;
+
+        System.out.println(area);
+
+    }
+}
+
